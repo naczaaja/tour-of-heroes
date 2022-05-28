dae486ca60d7aa0c06198546a81e6f42dd54c12c
Chirayu <chirayu.chomsri@gmail.com>
2022-05-28 10:28:44 +0700

    feat: dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes = HEROES.slice(1,5);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
